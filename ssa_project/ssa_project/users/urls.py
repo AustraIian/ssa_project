0b2f3c3c4f2c12c18391f01799dede25c1ad1d8f
Australian <70059943+AustraIian@users.noreply.github.com>
2024-11-07 22:34:15 +1100

    Group Join Requests

---
+++ ssa_project/ssa_project/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+
+from . import views
+
+urlpatterns = [
+    path("", views.user, name="user"),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+ 
+]
